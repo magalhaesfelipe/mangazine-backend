529cf2062ad9c5beae28bcda9565915875fb9ecd
lopesmfelipe <felipemlopes0009@gmail.com>
2024-09-22 16:43:45 -0400

    Removing some comments and examples in the List Model

--- src/models/listModel.ts
+++ src/models/listModel.ts
@@ -12,25 +12,3 @@ const listSchema = new mongoose.Schema(
 const List = mongoose.model('List', listSchema);
 
 export default List;
-
-/*
-
-In this example:
-
-const List = require('./models/list');
-
-List.findOne({ name: 'My List' })
-  .populate('titles') // Populate the 'titles' field with actual title documents
-  .exec((err, list) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(list);
-  });
-
-findOne is used to find a single list document based on some query criteria.
-populate('titles') tells Mongoose to replace the ObjectIds in the titles field with actual title documents.
-This makes it convenient to work with related documents in Mongoose, as you can retrieve the related documents in a single query rather than manually fetching them separately.
-
-*/
