5a4358bb2bc372231f3a13c0d13b61b6b7784f38
lopesmfelipe <felipemlopes0009@gmail.com>
2024-09-28 11:19:11 -0400

    Updating get manga endpoint in MangaRoutes

--- src/routes/mangaRoutes.ts
+++ src/routes/mangaRoutes.ts
@@ -14,7 +14,7 @@ router.route('/get-all-mangas').get(getAllMangas);
 router.route('/:mangaId').get(getMangaById);
 router.route('/search/:mangaName').get(getMangaByName);
 router.route('/update-manga/:mangaId').patch(updateManga);
-router.route('/create-title').post(createManga);
+router.route('/create-manga').post(createManga);
 router.route('/delete/:mangaId').delete(deleteManga);
 
 export default router;
