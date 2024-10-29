308f5cb062819f682fe9a9c973ed4c9961d0e9a0
magalhaesfelipe <felipemlopes0009@gmail.com>
2024-10-29 06:34:06 -0400

    Updating the author model

---
+++ src/models/authorModel.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from 'mongoose';
+
+const authorSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'An Author must have a name'],
+    unique: true,
+    trim: true,
+    validate: {
+      validator: (value: string) => {
+        return !!(value && value.trim().length > 0);
+      },
+      message: `The author's name cannot be empty`,
+    },
+  },
+  bio: {
+    type: String,
+    trim: true,
+  },
+  dateOfBirth: {
+    type: Date,
+  },
+  placeOfBirth: {
+    type: String,
+    trim: true,
+  },
+  photo: {
+    type: String,
+  },
+  works: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'worksModel',
+    },
+  ],
+  worksModel: [
+    {
+      type: String,
+      required: true,
+      enum: ['Manga', 'Book'],
+    },
+  ],
+  otherPhotos: [{ type: String }],
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+export default Author;
